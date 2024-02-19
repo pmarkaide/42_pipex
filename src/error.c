039bd088c8b630c063e8f9ab084d7359f39d4ea3
Pablo Markaide <pmarkaid@student.hive.fi>
2024-02-19 16:09:10 +0200

    refactor: error messages use perror n ow

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:25:34 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/02/16 16:24:13 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/02/19 14:29:23 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@
 
 int	error_1(const char *error_msg)
 {
-	ft_printf("%sError\n%s%s\n", RED, DEFAULT, error_msg);
+	ft_printf("%sError\n%s\n", RED, DEFAULT);
+	perror(error_msg);
 	return (1);
 }
 
 char	*error_null(const char *error_msg)
 {
-	ft_printf("%sError\n%s%s\n", RED, DEFAULT, error_msg);
+	ft_printf("%sError\n%s\n", RED, DEFAULT);
+	perror(error_msg);
 	return (NULL);
 }
 
 void	exit_1(const char *error_msg)
 {
-	ft_printf("%sError\n%s%s\n", RED, DEFAULT, error_msg);
+	ft_printf("%sError\n%s\n", RED, DEFAULT);
+	perror(error_msg);
 	exit(1);
 }
