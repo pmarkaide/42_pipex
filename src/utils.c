ad5f3a5306d89424a08aaa3127e9a218834f3d57
Pablo Markaide <pmarkaid@student.hive.fi>
2024-02-19 12:10:59 +0200

    feat: parse_cmd_args and parse_paths functional

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 12:19:41 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/02/18 15:53:57 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/02/19 12:10:11 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,37 @@ char **parse_cmd_args(char *arg)
 	while (arg[i] != '\0')
 	{
 		if(ft_isspace(arg[i]))
-			arg[i++] = ' ';
+			arg[i] = ' ';
+		i++;
 	}
 	cmd = ft_split(arg, ' ');
-	ft_print_array(cmd);
 	return(cmd);	
+}
+
+char **parse_paths(char **envp)
+{
+	int i;
+	char **paths;
+
+	paths=NULL;
+	i = 0;
+	while (envp[i] != '\0')
+	{
+		if(ft_strnstr(envp[i], "PATH=", 5))
+			{
+				paths = ft_split(envp[i] + 5,':');
+			}
+		i++;
+	}
+	return(paths);
 }
 
 void init_struct(t_data	*data, char **argv, char **envp)
 {
 	if (pipe(data->pipe_fd) == -1)
 		error_1("pipe failed");
-	data ->cmd1 = parse_cmd_args(argv[1]);
-	data ->cmd2 = parse_cmd_args(argv[2]);
-	//data->paths = parse_paths(envp)
-	
+	(void)envp;
+	data ->cmd1 = parse_cmd_args(argv[2]);
+	data ->cmd2 = parse_cmd_args(argv[3]);
+	data->paths = parse_paths(envp);
 }
